37b17814bb1644251b72c9f615290e4dc3a28b87
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-03-22 08:04:14 +0200

    Add SendRequest API view and update Ask model foreign keys

---
+++ save_lives/asks/api/sendreq/views.py
@@ -0,0 +1,43 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from asks.models import Ask
+from users.models import Donor, Receiver
+
+
+class SendRequest(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        # Authorize the user as a receiver
+        user = request.user
+        if not user.user_type == 'receiver':
+            return Response({'error': 'User is not a receiver'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        # Get the data
+        donor_id = request.data['donor_id']
+        needed_blood_type = request.data['needed_blood_type']
+        proposed_blood_type = request.data['proposed_blood_type']
+
+        try:
+            donor = Donor.objects.get(id=donor_id)
+        except Donor.DoesNotExist:
+            return Response({'error': 'Donor not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            receiver = Receiver.objects.get(user=user)
+        except Receiver.DoesNotExist:
+            return Response({'error': 'Receiver not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        # Make an Ask request
+        ask = Ask.objects.create(
+            receiver=receiver,
+            donor=donor,
+            needed_blood_type=needed_blood_type,
+            proposed_blood_type=proposed_blood_type
+        )
+        ask.save()
+
+        return Response({'message': 'Donataion request sent'}, status=status.HTTP_201_CREATED)
